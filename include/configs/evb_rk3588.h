be21c34067d6e3a2036ce70d571700170c59d270
Jonas Karlman <jonas@kwiboo.se>
2023-01-26 21:47:41 +0000

    WIP: rockchip: board: add rk3588-evb

---
+++ include/configs/evb_rk3588.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2016 Rockchip Electronics Co., Ltd
+ */
+
+#ifndef __CONFIGS_RK3588_EVB_H
+#define __CONFIGS_RK3588_EVB_H
+
+#include <configs/rk3588_common.h>
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#endif
